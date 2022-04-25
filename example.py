311754569bc7fad64aaa36e42be809023fd5a086
oldmartijntje <oldmartijntje@gmail.com>
2022-04-25 13:13:08 +0200

    test commit

--- example.py
+++ example.py
@@ -1,9 +1,19 @@
 import rpg
+
 player = rpg.System()
-player.startGame('Create')
+player.startGame('Play',1)
 # Jouw python instructies zet je vanaf hier:
 
-
+    #0 air
+    #1 wall
+    #2 start
+    #3 next level
+    #4 high chance of loot
+    #5 high chance of enemy
+    #6 sign
+    #7 npc
+    #8 nothing able to spawn
+    #9 bossfight
 
 
 
--- rpg.py
+++ rpg.py
@@ -10,50 +10,98 @@ from tkinter import ttk
     #4 high chance of loot
     #5 high chance of enemy
     #6 sign
+    #7 npc
+    #8 nothing able to spawn
+    #9 bossfight
+maxTypes = 9
 
+colors =['white','black','green', 'blue', 'pink', 'red', 'brown', 'orange', 'white', 'purple']
 class System:
 
     
     _defaultlevels = [
-    [[1,1,1,1,1,1,1,1,1,1], [1,0,0,0,0,0,0,0,0,1],[1,0,0,0,0,0,0,0,0,1],[1,0,0,0,0,0,0,0,0,1],[1,0,0,0,0,0,0,0,0,1],[1,0,0,0,0,0,0,0,0,1],[1,0,0,0,0,0,0,0,0,1],[1,0,0,0,0,0,0,0,0,1],[1,0,0,0,0,0,0,0,0,1],[1,1,1,1,1,1,1,1,1,1]]]
+    [[1,1,1,1,1,1,1,1,1,1], [1,0,0,0,0,0,0,0,0,1],[1,0,0,0,0,0,0,0,0,1],[1,0,0,0,0,0,0,0,0,1],[1,0,0,0,0,0,0,0,0,1],[1,0,0,0,0,0,0,0,0,1],[1,0,0,0,0,0,0,0,0,1],[1,0,0,0,0,0,0,0,0,1],[1,0,0,0,0,0,0,0,0,1],[1,1,1,1,1,1,1,1,1,1]],
+    [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]],
+    [[0, 0, 1, 0, 4, 5, 1, 4, 1, 2], [4, 0, 0, 4, 0, 0, 0, 0, 1, 0], [5, 4, 1, 0, 5, 4, 1, 0, 1, 0], [4, 0, 1, 0, 4, 0, 1, 0, 1, 0], [1, 0, 1, 5, 0, 4, 1, 3, 1, 0], [0, 5, 4, 1, 1, 1, 1, 1, 1, 0], [4, 0, 0, 1, 4, 0, 0, 4, 0, 0], [0, 0, 4, 1, 0, 4, 5, 0, 0, 0], [5, 0, 0, 0, 0, 5, 0, 5, 1, 0], [4, 0, 0, 1, 4, 0, 0, 4, 1, 3]],
+    [[4, 0, 4, 0, 1, 2, 1, 4, 5, 4], [4, 5, 0, 4, 1, 0, 1, 0, 4, 0], [0, 4, 0, 0, 0, 0, 1, 4, 5, 4], [0, 0, 4, 0, 1, 0, 1, 0, 4, 0], [4, 0, 5, 4, 1, 0, 0, 4, 5, 4], [1, 0, 1, 1, 1, 0, 1, 1, 1, 0], [0, 4, 0, 4, 1, 0, 1, 4, 0, 4], [4, 5, 4, 0, 1, 0, 1, 0, 5, 0], [0, 4, 0, 4, 0, 0, 1, 0, 4, 4], [4, 5, 4, 0, 1, 3, 1, 5, 0, 0]],
+    [[4, 0, 4, 1, 0, 2, 1, 4, 5, 4], [4, 5, 0, 1, 7, 0, 1, 0, 4, 0], [0, 4, 0, 0, 1, 0, 1, 4, 5, 4], [0, 0, 4, 0, 0, 0, 1, 0, 4, 0], [4, 0, 5, 0, 1, 0, 0, 4, 5, 4], [1, 0, 1, 1, 1, 0, 1, 1, 1, 0], [0, 4, 0, 4, 1, 0, 1, 4, 0, 4], [4, 5, 4, 0, 1, 0, 1, 0, 5, 0], [0, 4, 0, 4, 0, 0, 1, 0, 4, 4], [4, 5, 4, 0, 1, 3, 1, 5, 0, 7]],
+    [[1, 4, 0, 0, 5, 0, 4, 1, 0, 3], [0, 1, 1, 1, 0, 1, 1, 0, 0, 1], [0, 0, 0, 0, 0, 0, 0, 5, 0, 0], [2, 0, 1, 5, 0, 1, 0, 0, 1, 0], [0, 0, 4, 1, 0, 5, 1, 1, 4, 1], [0, 1, 1, 1, 0, 0, 0, 0, 0, 0], [0, 0, 5, 1, 4, 1, 5, 1, 0, 1], [0, 0, 0, 1, 1, 0, 0, 0, 0, 0], [0, 1, 0, 5, 0, 0, 1, 1, 0, 1], [4, 0, 0, 1, 4, 0, 5, 0, 0, 5]],
+    [[8, 8, 8, 8, 8, 8, 1, 2, 0, 0], [8, 8, 8, 8, 8, 8, 1, 0, 0, 0], [8, 8, 8, 8, 8, 8, 1, 0, 0, 0], [8, 8, 8, 8, 8, 8, 1, 0, 0, 0], [8, 8, 8, 8, 8, 8, 1, 6, 0, 0], [8, 8, 9, 8, 8, 8, 0, 0, 0, 0], [8, 8, 8, 8, 8, 8, 1, 0, 0, 0], [8, 8, 8, 8, 8, 8, 1, 0, 0, 0], [8, 8, 8, 8, 8, 8, 1, 0, 0, 4], [8, 8, 8, 8, 8, 8, 1, 4, 4, 4]],
+    [[0, 0, 0, 0, 1, 6, 4, 1, 4, 7], [0, 1, 1, 5, 1, 0, 5, 1, 0, 0], [5, 1, 3, 0, 1, 0, 1, 1, 1, 0], [0, 1, 1, 1, 1, 5, 0, 0, 1, 0], [0, 0, 4, 1, 4, 0, 1, 5, 0, 0], [0, 1, 0, 1, 0, 1, 1, 1, 0, 6], [4, 1, 0, 0, 5, 0, 0, 1, 0, 1], [4, 1, 1, 1, 1, 1, 0, 1, 0, 0], [0, 1, 3, 0, 1, 4, 0, 1, 1, 5], [2, 1, 1, 0, 0, 0, 1, 3, 0, 0]],
+    [[4, 5, 4, 0, 5, 0, 1, 4, 0, 0], [4, 4, 4, 1, 4, 5, 1, 5, 5, 4], [1, 1, 1, 1, 1, 0, 1, 4, 0, 0], [4, 0, 1, 0, 0, 0, 1, 1, 0, 1], [0, 5, 1, 0, 2, 0, 0, 0, 0, 0], [0, 4, 1, 0, 0, 4, 1, 1, 1, 5], [0, 1, 1, 1, 1, 1, 1, 0, 0, 0], [4, 0, 0, 1, 0, 0, 0, 4, 1, 0], [0, 5, 5, 0, 0, 5, 0, 0, 1, 0], [4, 0, 4, 1, 4, 0, 0, 5, 1, 3]],
+    [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1], [1, 2, 0, 0, 0, 0, 1, 4, 0, 1], [1, 1, 0, 1, 1, 0, 0, 0, 0, 1], [1, 6, 0, 4, 1, 0, 1, 5, 4, 1], [1, 4, 0, 0, 1, 5, 1, 0, 0, 1], [1, 1, 1, 0, 1, 0, 1, 1, 1, 1], [1, 4, 0, 5, 1, 0, 0, 0, 4, 1], [1, 4, 0, 0, 1, 0, 1, 5, 0, 1], [1, 0, 0, 4, 1, 3, 1, 0, 4, 1], [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
+    ]
     buttons = []
 
 
 
 
     def __init__(self, seed : int = 0):
-        global data, configSettings
+        self.window = tkinter.Tk()
         self.seed = seed
         random.seed = seed
-        configSettings = accounts_omac.configFileTkinter()
-        data = accounts_omac.defaultConfigurations.defaultLoadingTkinter(configSettings)
-        if data == False:
+        self.configSettings = accounts_omac.configFileTkinter()
+        self.data = accounts_omac.defaultConfigurations.defaultLoadingTkinter(self.configSettings)
+        random.randint(1,10)
+        self.nextStates = []
+        self.newState()
+        self.newState()
+        self.checkStates()
+
+        if self.data == False:
             exit()
 
-    def change(self,location):
+    def newState(self, modifier = 0):
+        for x in range(random.randint(1,10)+ modifier):
+            random.randint(1,10)
+        self.nextStates.append(random.getstate())
+        for x in range(random.randint(1,10)+ modifier):
+            random.randint(1,10)
+
+    def checkStates(self):
+        x = 0
+        while len(self.nextStates) > 2:
+            self.newState()
+        while self.nextStates[0] == self.nextStates[1]:
+            x += 1
+            self.nextStates.pop(0)
+            self.newState(x)
+
+    def loadState(self):
+        random.setstate(self.nextStates[0])
+        self.nextStates.pop(0)
+        self.checkStates()
+
+    def change(self,location, chosenLevel):
         x, y = location
-        self._defaultlevels[0][x][y]+= 1
-        if self._defaultlevels[0][x][y] > 6:
-            self._defaultlevels[0][x][y] = 0
+        self._defaultlevels[chosenLevel][x][y]+= 1
+        if self._defaultlevels[chosenLevel][x][y] > maxTypes:
+            self._defaultlevels[chosenLevel][x][y] = 0
+        self.buttons[x][y].configure(text=self._defaultlevels[chosenLevel][x][y], bg = colors[self._defaultlevels[chosenLevel][x][y]])
+        
 
-        exec(f'{location}.configure(text = defaultlevels[0][{x}][{y}])')
+    def startGame(self, mode = 'Play', chosenLevel = 0):
 
-    def startGame(self, mode = 'Play'):
-        global window
-        window = tkinter.Tk()
+        if type(chosenLevel) == list:
+            self._defaultlevels[0] = list(chosenLevel)
+            chosenLevel = 0
         self.buttons = []
         if mode == 'Create':
-            for x in range(len(self._defaultlevels[0])):
+            for x in range(len(self._defaultlevels[chosenLevel])):
                 self.buttons.append([])
-            for x in range(len(self._defaultlevels[0])):
-                for y in range(len(self._defaultlevels[0][x])):
+            for x in range(len(self._defaultlevels[chosenLevel])):
+                for y in range(len(self._defaultlevels[chosenLevel][x])):
                     cords = [x,y]
-                    self.buttons.append(ttk.Button(self, text=self._defaultlevels[0][x][y], command=lambda cords=cords:self.change(cords)))
-        window.mainloop()
+                    self.buttons[x].append(tkinter.Button(self.window, text=self._defaultlevels[chosenLevel][x][y],bg = colors[self._defaultlevels[chosenLevel][x][y]], command=lambda cords=cords:self.change(cords, chosenLevel)))
+                    self.buttons[x][y].grid(column=x, row=y, ipadx=20, ipady=10, sticky="EW")
+            tkinter.Button(self.window, text='export',command=lambda: print(self._defaultlevels[chosenLevel])).grid(column=0,row=x+1)
+        if mode == 'Play':
+            self.checkStates()
+            
+        self.window.mainloop()
 
     def exit(self):
-        global data
-        data = accounts_omac.saveAccount(data, configSettings)
+        self.data = accounts_omac.saveAccount(self.data, self.configSettings)
         exit()
 
 
